c4e50e447e51884fccf7774244cb4314e9136251
Soham Chaudhari <sohamchaudhari@Sohams-MacBook-Air.local>
2026-07-21 23:48:10 +0530

    Remove isAuth from public GET endpoints so unauthenticated users can view menus

--- backend/routes/shop.routes.js
+++ backend/routes/shop.routes.js
@@ -9,6 +9,6 @@ const shopRouter=express.Router()
 
 shopRouter.post("/create-edit",isAuth,upload.single("image"),createEditShop)
 shopRouter.get("/get-my",isAuth,getMyShop)
-shopRouter.get("/get-by-city/:city",isAuth,getShopByCity)
+shopRouter.get("/get-by-city/:city",getShopByCity)
 
 export default shopRouter
