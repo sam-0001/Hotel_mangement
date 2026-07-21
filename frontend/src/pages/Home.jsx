e4057b4b1de85aa4951376153bb375d281f0f665
Soham Chaudhari <sohamchaudhari@Sohams-MacBook-Air.local>
2026-07-21 23:52:29 +0530

    Fix unauthenticated null check in Home.jsx causing crash

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -8,9 +8,9 @@ function Home() {
     const {userData}=useSelector(state=>state.user)
   return (
     <div className='w-[100vw] min-h-[100vh] pt-[100px] flex flex-col items-center bg-[#fff9f6]'>
-      {userData.role=="user" && <UserDashboard/>}
-      {userData.role=="owner" && <OwnerDashboard/>}
-      {userData.role=="deliveryBoy" && <DeliveryBoy/>}
+      {(!userData || userData.role=="user") && <UserDashboard/>}
+      {userData?.role=="owner" && <OwnerDashboard/>}
+      {userData?.role=="deliveryBoy" && <DeliveryBoy/>}
     </div>
   )
 }
